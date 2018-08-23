9b9bd26feb9ceb2135ba49c8d9b1dc9b9027a0ed
pedromateo <pedrolmn@gmail.com>
2018-08-23 18:34:35 +0200

    x

--- python/src/datamodel.py
+++ python/src/datamodel.py
@@ -1,23 +1,19 @@
-#!/usr/bin/env ruby
 
-class Tablature
 
-    attr_accessor :band;
-    attr_accessor :song;
-    attr_accessor :content;
-    attr_accessor :multicol;
+class Tablature:
 
     # Create the object
-    def initialize(band = "", song = "", content = "", multicol = "")
-        @band = band
-        @song = song
-        @content = content
-        @multicol = multicol
-    end
+    def __init__(self, band = "", song = "", content = "", multicol = False):
+        self._band = band
+        self._song = song
+        self._content = content
+        self._multicol = multicol
 
     # returns tex code
-    def toTex
+    def toTex(self):
 
+        return "toTex method"
+        '''
         # header
         tex = "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n";
         tex += "\\newpage\n";
@@ -37,10 +33,13 @@ class Tablature
         end
 
         return tex;
-    end
+        '''
 
     # compares to another tablature
-    def equalTo(otherTablature)
+    def equalTo(self,otherTablature):
+
+        return "equalTo method"
+        '''
         if otherTablature.is_a?(Tablature)
             a = otherTablature.band.downcase + "" + otherTablature.song.downcase;
             b = @band.downcase + "" + @song.downcase;
@@ -50,32 +49,33 @@ class Tablature
             end
         end
         return false;
-    end
+        '''
 
-end
 
-class Chord
-
-    attr_accessor :name;
-    attr_accessor :value;
+class Chord:
 
     # Create the object
-    def initialize(band = "", song = "")
-        @name = name
-        @value = value
-    end
+    def __init__(self, band = "", song = ""):
+        self._name = name
+        self._value = value
 
     # returns tex code
-    def toTex
+    def toTex(self):
 
+        return "toTex method"
+        '''
         tex = "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n";
         # TODO
 
         return tex;
-    end
+        '''
 
     # compares to another chord
-    def equalTo(otherChord)
+    def equalTo(self,otherChord):
+
+        return "equalTo method"
+
+        '''
         if otherChord.is_a?(Chord)
             a = otherChord.name.downcase + "" + otherChord.value.downcase;
             b = @name.downcase + "" + @value.downcase;
@@ -84,26 +84,24 @@ class Chord
             end
         end
         return false;
-    end
+        '''
 
-end
 
 ###
 ### ############################################################################
 ###
 
+# test statements
 
-if __FILE__ == $0
+tb1 = Tablature("Oasis","Wonderwall","This is the content",False)
+tb2 = Tablature("Oasis","Wonderwall","This is a repetition",True)
+tb3 = Tablature("Daft Punk","Me","This is the content",True)
 
-    tb1 = Tablature.new("Oasis","Wonderwall","This is the content",false);
-    tb2 = Tablature.new("Oasis","Wonderwall","This is a repetition",true);
-    tb3 = Tablature.new("Daft Punk","Me","This is the content",true);
 
-    puts tb1.toTex
-    puts "------------------------------------------------"
-    puts tb3.toTex
-    puts "------------------------------------------------"
-    puts tb1.equalTo(tb2)
-    puts tb1.equalTo(tb3)
+tb1.toTex()
+"------------------------------------------------"
+tb3.toTex()
+"------------------------------------------------"
+tb1.equalTo(tb2)
+tb1.equalTo(tb3)
 
-end
